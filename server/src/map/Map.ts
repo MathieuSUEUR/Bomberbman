91922c5c5f08eaa46a5b247f1391cdf04df51ca8
lMasshu <baptistedevsh@gmail.com>
2026-09-16 19:16:28 +0200

    docs(map): Documentation de map.ts

--- server/src/map/Map.ts
+++ server/src/map/Map.ts
@@ -9,14 +9,30 @@ export class Map {
         this.grid = grid;
     }
 
+    /**
+     * méthode qui permet de recuperer le type d'une case
+     * @param x coordonnée x
+     * @param y coordonnée y
+     * @returns CellType le type de la case
+     */
     get(x: number, y: number): CellType | undefined {
         return this.grid[y]?.[x];
     }
 
+    /**
+     * méthode qui permet de recuperer la grille
+     * @returns CellType[][] la grille
+     */
     getGrid(): CellType[][] {
         return this.grid;
     }
 
+    /**
+     * méthode qui permet de modifier le type d'une case
+     * @param x coordonnée x
+     * @param y coordonnée y
+     * @param type type de la case
+     */
     setCell(x: number, y: number, type: CellType): void {
         if (this.grid[y]?.[x] !== undefined) {
             this.grid[y][x] = type;
